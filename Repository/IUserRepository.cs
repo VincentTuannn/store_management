adafd06f2d46b3882698e0477d0a94d681731134
VincentTuannn <vincenttuan1098@gmail.com>
2025-10-16 16:51:04 +0700

    adjust SQL and add Repository

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using store_management.Entity;
+
+namespace store_management.Repository
+{
+    public interface IUserRepository : IDisposable
+    {
+    IEnumerable<Users> GetUsers();
+    Users GetUserByID(int studentId);
+    void InsertUser(Users user);
+    void DeleteUser(int user_id);
+    void UpdateUser(Users user);
+    void Save();
+}
+}
--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace store_management.Repository
+﻿using store_management.Entity;
+
+namespace store_management.Repository
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository, IDisposable
     {
+        public void DeleteUser(int user_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Users GetUserByID(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Users> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertUser(Users user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(Users user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
